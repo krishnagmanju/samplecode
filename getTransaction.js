f22131442e9304db7fcf1dac8aa9a8d47f58f3f5
krishnagmanju <manju.gk@accubits.com>
2023-01-24 12:54:53 +0530

    node

--- getTransaction.js
+++
@@ -1,8 +0,0 @@
-import Web3 from'web3';
-
-async function App(){
-const web3 = new Web3('url..');
-const transaction = await web3.eth.getTransactionReceipt('0xc737531f440c8526c1b5eace67c08fd7ed188ea012a7b47db3f97469c9a01e8a');
-console.log(transaction)
-}
-App()
